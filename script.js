dfcf7f853d946fc42cc5b0283401d0b8d4589d3b
abisheksivaraj2001@gmail.com <abisheksivaraj2001@gmail.com>
2023-09-26 16:27:15 +0530

    completed

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-'use script';
-const input = document.querySelector("input");
+"use script";
+// select element
+const widthEl = document.getElementById("width-slide");
 
-input.oninput = () => {
-  document.body.style.backgroundColor = input.Value;
-};
+const radiusEl = document.getElementById("radius-slide");
+
+const colorEl = document.getElementById("color-box");
+
+const paddingEl = document.getElementById("padding-slide");
+
+const imageEl = document.querySelector(".image");
+
+const blurEl = document.getElementById("blur-slide");
+
+
+const jsEl = document.getElementById("js");
+
+
+
+const buildingEl = document.getElementById("building");
+
+widthEl.addEventListener("input", () => {
+  imageEl.style.width = widthEl.value + "rem";
+});
+
+radiusEl.addEventListener("input", () => {
+  buildingEl.style.borderRadius = radiusEl.value + "px";
+});
+
+colorEl.addEventListener("input", () => {
+  buildingEl.style.borderColor = colorEl.value;
+  jsEl.style.color = colorEl.value;
+});
+
+
+paddingEl.addEventListener("input", () => {
+  buildingEl.style.borderWidth = paddingEl.value + "px";
+});
+
+blurEl.addEventListener("input", () => {
+  imageEl.style.filter = `blur(${blurEl.value + "px"})`;
+});
